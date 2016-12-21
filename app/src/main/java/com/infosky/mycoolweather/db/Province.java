d0e50f1eaf8e4cfeb3db42fe4627316203aad75a
PineChang <1034638186@qq.com>
2016-12-21 11:02:37 +0800

    加入数据库和表等相关内容

---
+++ app/src/main/java/com/infosky/mycoolweather/db/Province.java
@@ -0,0 +1,37 @@
+package com.infosky.mycoolweather.db;
+
+import org.litepal.crud.DataSupport;
+
+/**
+ * Created by PineChang on 2016/12/21.
+ */
+public class Province extends DataSupport {
+
+    private  int id;
+    private String provinceName;
+    private int procinceCode;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getProvinceName() {
+        return provinceName;
+    }
+
+    public void setProvinceName(String provinceName) {
+        this.provinceName = provinceName;
+    }
+
+    public int getProcinceCode() {
+        return procinceCode;
+    }
+
+    public void setProcinceCode(int procinceCode) {
+        this.procinceCode = procinceCode;
+    }
+}
